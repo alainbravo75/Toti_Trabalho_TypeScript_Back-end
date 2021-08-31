5100a8a487ba4ba9e2e56ea5b2fc37773629f119
Alain <alinobravo3@gmail.com>
2021-08-31 18:19:51 -0300

    Deixei em comentário o primeiro algoritmo typescript e acrescentei o return circulo como number ou string

--- README.md.txt
+++ README.md.txt
@@ -1,1 +1,4 @@
-Começando a desenvolver um projeto de calculos de áreas de figuras geométricas
+Começando a desenvolver um projeto de calculos de áreas de figuras geométricas.
+
+Quando comecei a programar, comecei usando a classe e herança mas para os valores dos atributos estavão apresentando um erro e não consigui a determinar os valores opcionais.
+No final do código typescript deixei o primeiro algoritmo em comentário.
--- index.ts
+++ index.ts
@@ -97,7 +97,245 @@
 
         //calcular a área de um circulo A = pi * r²
         class CirculoGeo{
-            pi: number
+           private pi: number
+            r: number
+            constructor(pi: number, r: number){
+                this.pi = pi;
+                this.r = r;
+            }
+            getCirculo(): number| string{
+                let A = this.pi * (this.r * this.r) 
+                return "A área do circulo é igual a " + A + "cm"
+            }
+            }
+        const circuloGeo = new CirculoGeo(32, 24);
+        console.log(circuloGeo.getCirculo());
+
+
+            
+        /*
+        Inicialmente queria usar a classe com herança só que teve um erro persistente na declaração dos 
+        atributos é por isso que eu usei as calsses separadamente. 
+        Em baixo está o código inicial*/
+
+            /*
+         class Geometria {
+        a: number
+        b: number
+        h: number
+        pi: number
+        r: number
+        B: number
+
+        constructor(
+            a: number, b: number, h: number, pi: number, r: number, B: number){
+                this.a = a;
+                this.b = b;
+                this.h = h;
+                this.pi = pi;
+                this.r = r;
+                this.B = B;
+            }
+            getQuadrado(){
+        
+            }
+
+            getRectangulo(){
+        
+            }
+
+            getArea(){
+        
+            }
+
+            getParalelograma(){
+              
+            }
+
+            getTrapezio(){
+               
+            }
+
+            getCirculo(){
+    
+            }
+    }
+    class GeometriaQuadrdrado extends Geometria{
+        constructor(a: number)
+            {
+                super(a)
+            }
+            getQuadrado(): number|string{
+                let A = this.a * this.a
+                return ("O Quadrado de " + this.a + " é igual a " + A + "cm");
+            }
+        }
+        const geometriaQuadrdrado = new GeometriaQuadrdrado(22)
+        console.log(geometriaQuadrdrado.getQuadrado());
+
+
+        class RectanguloGeometria extends Geometria{
+            constructor(a: number, b: number){
+                    super(a, b)
+                }
+                getArea(): number|string{
+                    let A = this.a * this.b
+                    return "A área d rectagulo é igual a " + A + "cm";
+                }
+        }
+        const rectanguloGeometria= new RectanguloGeometria(17, 22)
+        console.log(rectanguloGeometria.getArea());
+        
+        class TrianguloGeometria extends Geometria{
+            constructor(b: number, h: number){
+                super(b, h);
+            }
+            getTriangulo(): number|string{
+                let A = (this.b * this.h)/2
+                return "A área do triangulo é igual a " + A + "cm²"
+            }
+        }
+        const trianguloGeometria = new TrianguloGeometria(12, 8);
+        console.log(trianguloGeometria.getTriangulo())
+
+        class Paralelograma extends Geometria{
+            constructor(b: number, h: number){
+                super(b, h)
+            }
+            getParalelograma(): number|string{
+                let A = this.b * this.h
+                return "A área do paralelograma é igual a " + A + "cm"
+            }
+        }
+        const paralelograma = new Paralelograma(15, 20);
+        console.log(paralelograma.getParalelograma());
+
+        class Trapezia extends Geometria{
+            constructor(B: number, b: number, h: number){
+                super(B, b, h)
+            }
+            getTrapezio(): number|string{
+                let A = ((this.B * this.b) / 2) * this.h
+                return "A área da Trapézia é igual a " + A + "cm"
+            }
+        }
+        const trapezia = new Trapezia(7, 9, 4);
+        console.log(trapezia.getTrapezio());
+
+        class Circulo extends Geometria{
+            constructor(pi: number, r: number){
+                super(pi, r)
+            }
+            getCirculo(): number{
+                let A = this.pi * (this.r * this.r) 
+                return A
+            }
+            }
+        const circulo = new Circulo(32, 24);
+        console.log(circulo.getCirculo());
+
+
+
+
+
+
+
+//Calcular a área de um quadrado A = a²
+
+        
+    class Quadrado {a: number  
+
+        constructor(a: number){
+                this.a = a;
+            }
+            getQuadrado(): number|string{
+                let A = this.a * this.a
+                return ("O Quadrado de " + this.a + " é igual a " + A + "cm");
+            }
+        }
+        const quadrdrado = new Quadrado(22)
+        console.log(quadrdrado.getQuadrado());
+
+        
+// Calcular a área de um rectángulo A = a x b
+
+        class Rectangulo {
+            a: number
+            b: number  
+
+            constructor(a: number, b: number){
+                    this.a = a;
+                    this.b = b;
+                }
+                getRectangulo(): number|string{
+                    let A = this.a * this.b
+                    return "A área do rectagulo é igual a " + A + "cm";
+                }
+            }
+            const rectangulo = new Rectangulo(17, 22);
+            console.log(rectangulo.getRectangulo());
+    
+    //Calcular a área de um triângulo A = (bxh)/2
+        
+        class Triangulo{
+            b: number
+            h: number
+            constructor(b: number, h: number){
+                this.b = b;
+                this.h = h;
+            }
+            getTriangulo(): number|string{
+                let A = (this.b * this.h)/2
+                return " A área do triangulo é igual a " + A + "cm²"
+            }
+        }
+        const triangulo = new Triangulo(12, 8);
+        console.log(triangulo.getTriangulo())
+
+
+        // Calcular a área de um paralelograma A = bxh
+
+        class GeoParalelograma { 
+            b: number
+            h: number
+            constructor(b: number, h: number){
+                this.b = b;
+                this.h = h;
+            }
+            getParalelograma(): number|string{
+                let A = this.b * this.h
+                return "A área do paralelograma é igual a " + A + "cm"
+            }
+        }
+        const geoParalelograma = new GeoParalelograma(15, 20);
+        console.log(paralelograma.getParalelograma());
+
+
+
+
+        //clacular a área de uma trapézia A = (B+b/2)xh
+        class GeometriaTrapezia{
+            B: number
+            b: number
+            h: number
+            constructor(
+                B: number, b: number, h: number){
+                this.B = B;
+                this.b = b;
+                this.h = h;
+            }
+            getTrapezio(): number|string{
+                let A = ((this.B + this.b) / 2) * this.h
+                return "A área da Trapézia é igual a " + A + "cm"
+            }
+        }
+        const geometriaTrapezia = new GeometriaTrapezia(7, 9, 4);
+        console.log(trapezia.getTrapezio());
+
+
+        //calcular a área de um circulo A = pi * r²
+        class CirculoGeo{
+            private pi: number
             r: number
             constructor(pi: number, r: number){
                 this.pi = pi;
@@ -111,3 +349,4 @@
         const circuloGeo = new CirculoGeo(32, 24);
         console.log(circuloGeo.getCirculo());
 
+        */
